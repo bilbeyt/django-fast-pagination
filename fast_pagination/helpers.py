f9af4aa4d683b23093482083016a86f6ddaa3b61
Tolga Bilbey <tolga.bilbey@metglobal.com>
2020-03-15 16:24:44 +0300

    Using Django Paginator to handle orphans and availability

--- fast_pagination/helpers.py
+++ fast_pagination/helpers.py
@@ -1,8 +1,28 @@
+import inspect
+
+from django.core.paginator import Paginator, Page
 from django.db.models.query import QuerySet
-from django.core.paginator import Paginator
+from django.utils.functional import cached_property
+from django.utils.inspect import method_has_no_args
 
 
 class FastPaginator(Paginator):
+
+    def __init__(self, object_list, per_page, orphans=0,
+                 allow_empty_first_page=True):
+        super().__init__(object_list, per_page, orphans, allow_empty_first_page)
+        self.ids = object_list
+        if isinstance(object_list, QuerySet):
+            self.ids = list(self.object_list.values_list('id', flat=True))
+
+    @cached_property
+    def count(self):
+        c = getattr(self.object_list, 'count', None)
+        if callable(c) and not inspect.isbuiltin(c) \
+            and method_has_no_args(c) \
+            and not isinstance(self.object_list, QuerySet):
+            return c()
+        return len(self.ids)
 
     def page(self, number):
         number = self.validate_number(number)
@@ -12,6 +32,15 @@ class FastPaginator(Paginator):
             top = self.count
         object_list = self.object_list[bottom:top]
         if isinstance(self.object_list, QuerySet):
-            ids = self.object_list.values_list('id', flat=True)
+            ids = self.ids[bottom:top]
             object_list = self.object_list.filter(id__in=ids)
         return self._get_page(object_list, number, self)
+
+    def _get_page(self, *args, **kwargs):
+        return FastPage(*args, **kwargs)
+
+
+class FastPage(Page):
+
+    def __len__(self):
+        return len(self.paginator.ids)
--- fast_pagination/tests.py
+++ fast_pagination/tests.py
@@ -1,14 +1,14 @@
 import unittest
-from helpers import (
-    FastPaginator, InvalidObjectListException)
+from helpers import FastPaginator
 
 
 class FastPaginatorTestMethods(unittest.TestCase):
 
     def test_queryset_valid(self):
         example_list = ['1', '2', '3']
-        with self.assertRaises(InvalidObjectListException):
-            FastPaginator(example_list, 2)
+        with self.assertRaises(Exception):
+            paginator = FastPaginator(example_list, 2)
+            paginator.page(3)
 
 
 if __name__ == '__main__':
